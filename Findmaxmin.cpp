b943b4c0e219c73c89ff1f1260b78d7040c8b2ae
xckx1019 <yannimylove@hotmail.com>
2017-10-26 22:30:46 +0800

    findBSTminmax
    findBSTminmax

---
+++ Findmaxmin.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *getNewNode(int data)
+{
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->right = NULL;
+    newNode->left = NULL;
+    return newNode;
+}
+
+Node *Insert(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = getNewNode(data);
+    }
+    else if (data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+bool Search(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    else if (root->data == data)
+    {
+        return true;
+    }
+    else if (data <= root->data)
+    {
+        return Search(root->left, data);
+    }
+    else
+    {
+        return Search(root->right, data);
+    }
+}
+
+int FindMin(Node *root)
+{
+    Node *current = root;
+    while (current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current->data;
+}
+
+int FindMax(Node *root)
+{
+    Node *current = root;
+    while (current->right != NULL)
+    {
+        current = current->right;
+    }
+    return current->data;
+}
+
+int main()
+{
+    Node *root = NULL; // Creating an empty tree
+    /*Code to test the logic*/
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    int a = FindMin(root);
+    cout << a << endl;
+    int b = FindMax(root);
+    cout << b << endl;
+    // Ask user to enter a number.
+    int number;
+    cout << "Enter number be searched\n";
+    cin >> number;
+    //If number is found, print "FOUND"
+    if (Search(root, number) == true)
+        cout << "Found\n";
+    else
+        cout << "Not Found\n";
+    return 0;
+}
